cd2ebf03e40df678223ff28903b773ae7f7d402e
vitroid <vitroid@gmail.com>
2022-10-29 15:49:42 +0900

    .

---
+++ atomic/sketch.js
@@ -0,0 +1,74 @@
+x = 300.0
+y = 50
+vx = 0.0
+vy = 0.0
+mass = 50
+x2 = 50.0
+y2 = 300.0
+vx2 = 0
+vy2 = 0
+sig = 50
+sig2 = sig*sig
+sig6 = sig2*sig2*sig2
+sig12 = sig6*sig6
+eps = 100000.0
+
+dt = 0.01
+
+function setup(){
+    createCanvas(600,600)
+    frameRate(30)
+    mouseX = 300
+    mouseY = 300
+}
+
+
+function force_LJ(x, y, x2, y2){
+    let dx = x2 - x
+    let dy = y2 - y
+    let d2 = dx*dx + dy*dy
+    let d6 = d2*d2*d2
+    let d12 = d6*d6
+    let f = 4*eps*(-12*sig12/d12+6*sig6/d6)/d2
+    return [f*dx, f*dy]
+}
+
+function draw(){
+    // 力の計算
+    var f = force_LJ(x, y, mouseX, mouseY)
+    fx01 = f[0]
+    fy01 = f[1]
+    f = force_LJ(x2, y2, mouseX, mouseY)
+    fx02 = f[0]
+    fy02 = f[1]
+    f = force_LJ(x, y, x2, y2)
+    fx12 = f[0]
+    fy12 = f[1]
+    forcex = fx01 - fx12
+    forcey = fy01 - fy12
+    forcex2 = fx02 + fx12
+    forcey2 = fy02 + fy12
+    // 移動
+    vx = vx + forcex / mass * dt / 2
+    vy = vy + forcey / mass * dt / 2
+    vx2 = vx2 + forcex2 / mass * dt / 2
+    vy2 = vy2 + forcey2 / mass * dt / 2
+    x = x + vx * dt
+    y = y + vy * dt
+    x2 = x2 + vx2 * dt
+    y2 = y2 + vy2 * dt
+    vx = vx + forcex / mass * dt / 2
+    vy = vy + forcey / mass * dt / 2
+    vx2 = vx2 + forcex2 / mass * dt / 2
+    vy2 = vy2 + forcey2 / mass * dt / 2
+    // 表示
+    background(200)
+    stroke(0)
+    line(x,y,mouseX,mouseY)
+    line(x2,y2,mouseX,mouseY)
+    line(x,y,x2, y2)
+    fill(255)
+    ellipse(mouseX,mouseY,50,50)
+    ellipse(x,y,50,50)
+    ellipse(x2,y2,50,50)
+}
---
+++ follow-mouse/sketch.js
@@ -0,0 +1,11 @@
+function setup(){
+    createCanvas(600,600)
+    frameRate(30)
+}
+function draw(){
+    // 表示
+    background(200)
+    stroke(0)
+    fill(255)
+    ellipse(mouseX,mouseY,20,20)
+}
